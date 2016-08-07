072814eb5abe3d220d1cfc68fbf4e81736e608e4
tomahawk <tmutunhire@gmail.com>
2016-08-07 05:31:14 +0200

    version 1.0

--- FileDownloader.js
+++ FileDownloader.js
@@ -1,16 +1,34 @@
-function getFileName(link) { 
-  return link.innerHTML.toString().concat(".cpp"); 
+console.log("Usage, invoke function:  downloadFiles(extension, containerID) where extension is the file extension you want files saved as, less the '.', so e.g. 'cpp' for '.cpp' file ending");
+console.log("The optional parameter containerID is the HTML ID of the element think div or other, e.g. 'myId', without the hash, which wraps the area from which you want to download files, in case you do not want the selection to be the entire page.");
+function getFileName(link, extension, num) { 
+  return link.innerHTML.toString().concat(num).concat(".").concat(extension); 
 }
 
-numLinks = document.links.length;
 
-alert("Now downloading " + numLinks + "documents found");
+function announceDownload(numLinks, extension) {
+  alert("Now downloading " + numLinks + "documents found" + ", using extension: " + extension);
+}
 
-for(i=0; i < 350; i++) { 
-  af = document.links[i]; 
-  console.log(i + ": \n"); 
-  console.log(af); 
-  af.setAttribute("download", getFileName(af)); 
-  af.click(); 
+
+function downloadFiles(extension, containerID) {
+  numLinks = document.links.length;
+  if(containerID) {
+      numLinks = $("#".concat(containerID)).find("a").length;
+  }
+  
+  announceDownload(numLinks, extension);
+  
+  for(i=0; i < numLinks; i++) { 
+    
+    af = document.links[i]; 
+    if(containerID) {
+      af = $("#".concat(containerID)).find("a")[i];
+    }
+    console.log(i + ": \n"); 
+    console.log(af); 
+    af.setAttribute("download", getFileName(af, extension, i.toString())); 
+    af.click(); 
+    
+  }
   
 }
