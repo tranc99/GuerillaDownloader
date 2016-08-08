f256064dd8fdc627a117428ee65122c4d2096d9e
tomahawk <tmutunhire@gmail.com>
2016-08-08 18:38:36 +0200

    make the downloader work when there is no Javascript preloaded on the page

--- FileDownloader.js
+++ FileDownloader.js
@@ -1,5 +1,14 @@
 console.log("Usage, invoke function:  downloadFiles(extension, containerID) where extension is the file extension you want files saved as, less the '.', so e.g. 'cpp' for '.cpp' file ending");
 console.log("The optional parameter containerID is the HTML ID of the element think div or other, e.g. 'myId', without the hash, which wraps the area from which you want to download files, in case you do not want the selection to be the entire page.");
+
+// first load jQuery on the page
+
+ans = document.createElement('script');
+ans.setAttribute("src", "https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.0/jquery.min.js");
+document.body.appendChild(ans);
+
+
+
 function getFileName(link, extension, num) { 
   return link.innerHTML.toString().concat(num).concat(".").concat(extension); 
 }
